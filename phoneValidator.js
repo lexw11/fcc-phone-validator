a8175ea77295512eb2ab14eb2c36d7007eb162c2
lexw11 <36146160+lexw11@users.noreply.github.com>
2018-08-25 22:14:29 -0700

    Created and connected html and js files.

---
+++ phoneValidator.js
@@ -0,0 +1,11 @@
+function telephoneCheck(str) {
+	// Regex reqs:
+	// - Must have 2 sets of 3 digits each, followed by 1 set of 4 digits
+	// - Can start with 1
+	// - 1st set can be surrounded by parentheses, but must be a complete pair
+	// - Sets can be separated by spaces, hyphens, or nothing
+	// 		- But first sets can only be separated by hyphens if last two sets are?
+
+}
+
+telephoneCheck("555-555-5555");
