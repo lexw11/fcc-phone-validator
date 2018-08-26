0143ba6d921f4269f5aabdd3ba59830d3efe7578
lexw11 <36146160+lexw11@users.noreply.github.com>
2018-08-26 09:30:23 -0700

    Combine area code regex with full phone number regex

--- phoneValidator.js
+++ phoneValidator.js
@@ -5,15 +5,17 @@ function telephoneCheck(str) {
 	// - 1st set can be surrounded by parentheses, but must be a complete pair
 	// - Sets can be separated by spaces, hyphens, or nothing
 	// 		- But first sets can only be separated by hyphens if last two sets are?
-	let regex = /^(1[ -]?)?\d{3}.*[ -]?\d{3}[ -]?\d{4}$/;
+	// let regex = /^(1[ -]?)?\d{3}.*[ -]?\d{3}[ -]?\d{4}$/;
+	let regex = /^(1[ -]?)?(\((?=\d{3}\)))?\d{3}((?<=\(\d{3})\))?[ -]?\d{3}[ -]?\d{4}$/;
 	
-	let parenRe = /^(\((?=\d{3}\)))?\d{3}((?<=\(\d{3})\))?$/;
-	let parenStr = "(555)";
-	console.log(parenStr.match(parenRe));
-	return parenRe.test(parenStr);
+	// let parenRe = /^(\((?=\d{3}\)))?\d{3}((?<=\(\d{3})\))?$/;
+	// // let parenRe = /^((\()(?=(\d{3})(\))))?\d{3}((?<=\2\3)\4)?$/;
+	// let parenStr = "555)";
+	// console.log(parenStr.match(parenRe));
+	// return parenRe.test(parenStr);
 	
-	// console.log(str.match(regex));
-	// return regex.test(str);
+	console.log(str.match(regex));
+	return regex.test(str);
 }
 
-console.log(telephoneCheck("555-555 5555"));
+console.log(telephoneCheck("1 (555) 555 5555"));
