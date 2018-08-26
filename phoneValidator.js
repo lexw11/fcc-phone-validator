25f6ae7c1554c797aa095bc19e2218f7c914926f
lexw11 <36146160+lexw11@users.noreply.github.com>
2018-08-26 12:24:20 -0700

    Clean up code

--- phoneValidator.js
+++ phoneValidator.js
@@ -1,21 +1,31 @@
 function telephoneCheck(str) {
-	// Regex reqs:
-	// - Must have 2 sets of 3 digits each, followed by 1 set of 4 digits ==> \d{3} \d{3} \d{4}
-	// - Can start with 1 ==> ^1?
-	// - 1st set can be surrounded by parentheses, but must be a complete pair
-	// - Sets can be separated by spaces, hyphens, or nothing
-	// 		- But first sets can only be separated by hyphens if last two sets are?
-	// let regex = /^(1[ -]?)?\d{3}.*[ -]?\d{3}[ -]?\d{4}$/;
-	let regex = /^(1[ -]?)?(\((?=\d{3}\)))?\d{3}((?<=\(\d{3})\))?[ -]?\d{3}[ -]?\d{4}$/;
-	
-	// let parenRe = /^(\((?=\d{3}\)))?\d{3}((?<=\(\d{3})\))?$/;
-	// // let parenRe = /^((\()(?=(\d{3})(\))))?\d{3}((?<=\2\3)\4)?$/;
-	// let parenStr = "555)";
-	// console.log(parenStr.match(parenRe));
-	// return parenRe.test(parenStr);
-	
-	console.log(str.match(regex));
+	// Start regex expression
+	let regexStr =
+		// String can start with 1 
+		'^(1[ -]?)?'
+		// First set of 3 digits, parentheses optional
+		+ '(\\((?=\\d{3}\\)))?\\d{3}((?<=\\(\\d{3})\\))?'
+		// Second and third sets of digits (lengths of 3 and 4), optionally separated by hyphens or spaces
+		+ '[ -]?\\d{3}[ -]?\\d{4}$';
+	let regex = new RegExp(regexStr);
+
 	return regex.test(str);
 }
 
-console.log(telephoneCheck("1 (555) 555 5555"));
+let testArr = ['555-555-5555',
+'1 555-555-5555',
+'1 (555) 555-5555',
+'5555555555',
+'555-555-5555',
+'(555)555-5555',
+'1(555)555-5555',
+'555-5555',
+'5555555',
+'1 555)555-5555',
+'1 555 555 5555',
+'1 456 789 4444',
+'123**&!!asdf#']
+
+testArr.forEach( function(el) {
+	console.log(telephoneCheck(el));
+});
